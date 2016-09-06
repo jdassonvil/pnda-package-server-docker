dcacfa0e55dccb2ff3f51d61c949fed57f8a5900
Jerome Dassonville <jdassonv@cisco.com>
2016-09-06 15:14:36 +0200

    update kafka setup
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

--- build.d/platform_kafka.sh
+++ build.d/platform_kafka.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-echo "Step 9: Building Kafka manager" >> $LOG_FILE
+echo "Building Kafka manager"
+PACKAGE_SERVER="http://192.168.1.205:3535"
 VERSION=1.3.1.6
 KAFKA_MANAGER="$PWD/kafka-manager-${VERSION}"
+
 if [ ! -d $KAFKA_MANAGER ]; then
-  echo "downloading kafka manager $VERSION in $PWD" >> $LOG_FILE
+  echo "downloading kafka manager $VERSION in $PWD"
   wget https://github.com/yahoo/kafka-manager/archive/${VERSION}.tar.gz
   tar xzf ${VERSION}.tar.gz
 else
-  echo "	KAFKA_MANAGER is already set as $KAFKA_MANAGER" >> $LOG_FILE
+  echo "	KAFKA_MANAGER is already set as $KAFKA_MANAGER"
 fi
 if [ ! -f .sbt/0.13/local.sbt ]; then
 	mkdir -p .sbt/0.13
@@ -17,14 +19,23 @@ fi
 cd $KAFKA_MANAGER
 sbt clean dist
 cd target/universal/
+
 if [ ! -f kafka-manager-${VERSION}.zip ]; then
-	echo "	Error building kafka manager" >> $LOG_FILE
+	echo "	Error building kafka manager"
 	exit 1
 else
-	echo "	Build done: kafka-manager-${VERSION}.zip" >> $LOG_FILE
+	echo "	Build done: kafka-manager-${VERSION}.zip"
 fi
+
 sha512sum kafka-manager-${VERSION}.zip > kafka-manager-${VERSION}.zip.sha512.txt
 mkdir -p "$RELEASE_PATH/packages/platform/releases/kafka-manager"
 mv kafka-manager-${VERSION}.zip $RELEASE_PATH/packages/platform/releases/kafka-manager/kafka-manager-${VERSION}.zip
 mv kafka-manager-${VERSION}.zip.sha512.txt $RELEASE_PATH/packages/platform/releases/kafka-manager/kafka-manager-${VERSION}.zip.sha512.txt
 cd ../../..
+
+# Publish to package server
+
+echo "curl -X POST --data-binary @kafka-manager-${VERSION}.tar.gz $PACKAGE_SERVER/packages/platform/releases/kafka-manager/kafka-manager-${VERSION}.tar.gz"
+curl -X POST --data-binary @kafka-manager-${VERSION}.tar.gz $PACKAGE_SERVER/packages/platform/releases/kafka-manager/kafka-manager-${VERSION}.tar.gz
+echo "curl -X POST --data-binary @kafka-manager-${VERSION}.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/kafka-manager/kafka-manager-${VERSION}.tar.gz.sha512.txt"
+curl -X POST --data-binary @kafka-manager-${VERSION}.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/kafka-manager/kafka-manager-${VERSION}.tar.gz.sha512.txt
