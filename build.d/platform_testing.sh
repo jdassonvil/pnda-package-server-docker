985d9f7d7b616cc1b450813bf2eff142fd8fe74f
Jerome Dassonville <jdassonv@cisco.com>
2016-09-06 10:39:48 +0200

    Remove log file requirement
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

--- build.d/platform_testing.sh
+++ build.d/platform_testing.sh
@@ -3,17 +3,17 @@
 PLATFORM_TESTING="$GITHUB_ENDPOINT/platform-testing.git"
 PACKAGE_SERVER="http://192.168.1.205:3535"
 
-echo "Step 1: cloning platform-testing $PLATFORM_TESTING in $PWD" >> $LOG_FILE
+echo "Step 1: cloning platform-testing $PLATFORM_TESTING in $PWD"
 
 if [ ! -d "$PWD/platform-testing" ]; then
-	echo "	cloning $BRANCH for platform-testing" >> $LOG_FILE
+	echo "	cloning $BRANCH for platform-testing"
 	git clone $PLATFORM_TESTING
 	if [ ! -d "$PWD/platform-testing" ]; then
-		echo "Error clonning platform-testing" >> $LOG_FILE
+		echo "Error clonning platform-testing"
 		exit 1
 	fi
 else
-	echo "	getting $BRANCH for platform-testing" >> $LOG_FILE
+	echo "	getting $BRANCH for platform-testing"
 fi
 cd platform-testing
 git checkout $BRANCH
@@ -27,16 +27,16 @@ mvn versions:set -DnewVersion=$VERSION
 mvn clean package
 cd target
 if [ ! -f platform-testing-cdh-$VERSION.tar.gz ]; then
-	echo "	Error building platform-testing" >> $LOG_FILE
+	echo "	Error building platform-testing"
 	exit 1
 else
-	echo "	Build done: platform-testing-cdh-$VERSION.tar.gz" >> $LOG_FILE
+	echo "	Build done: platform-testing-cdh-$VERSION.tar.gz"
 fi
+
 sha512sum platform-testing-cdh-$VERSION.tar.gz > platform-testing-cdh-$VERSION.tar.gz.sha512.txt
 sha512sum platform-testing-general-$VERSION.tar.gz > platform-testing-general-$VERSION.tar.gz.sha512.txt
-mkdir -p "$RELEASE_PATH/packages/platform/releases/platform-testing"
 
-
+# Publish to package server
 curl -X POST -o platform-testing-cdh-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/platform-testing/platform-testing-cdh-$VERSION.tar.gz
 curl -X POST -o platform-testing-general-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/platform-testing/platform-testing-general-$VERSION.tar.gz
 curl -X POST -o platform-testing-cdh-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/platform-testing/platform-testing-cdh-$VERSION.tar.gz.sha512.txt
