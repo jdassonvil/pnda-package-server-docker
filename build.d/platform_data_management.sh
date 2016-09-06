1d7f90ce04b77fe9b6e8f75744a7c00a4cd8c7af
Jerome Dassonville <jdassonv@cisco.com>
2016-09-06 14:40:34 +0200

    Update platform data management
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

--- build.d/platform_data_management.sh
+++ build.d/platform_data_management.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 PLATFORM_DATA_MANAGEMENT="$GITHUB_ENDPOINT/platform-data-mgmnt.git" 
-echo "Step 2: cloning platform-data-mgmnt $PLATFORM_DATA_MANAGEMENT in $PWD" >> $LOG_FILE
+PACKAGE_SERVER="http://192.168.1.205:3535"
+
+echo "Cloning platform-data-mgmnt $PLATFORM_DATA_MANAGEMENT in $PWD"
 git clone $PLATFORM_DATA_MANAGEMENT
 if [ ! -d "$PWD/platform-data-mgmnt" ]; then
-	echo "	cloning $BRANCH for platform-data-mgmnt" >> $LOG_FILE
+	echo "	cloning $BRANCH for platform-data-mgmnt"
 	git clone $PLATFORM_DATA_MANAGEMENT
 	if [ ! -d "$PWD/platform-data-mgmnt" ]; then
-		echo "Error clonning platform-data-mgmnt" >> $LOG_FILE
+		echo "Error clonning platform-data-mgmnt"
 		exit 1
 	fi
 else
-	echo "	getting $BRANCH for platform-data-mgmnt" >> $LOG_FILE
+	echo "	getting $BRANCH for platform-data-mgmnt"
 fi
 cd platform-data-mgmnt
 git checkout $BRANCH
@@ -26,15 +28,21 @@ mvn versions:set -DnewVersion=$VERSION
 mvn clean package
 cd target
 if [ ! -f data-service-$VERSION.tar.gz ]; then
-	echo "	Error building data-service" >> $LOG_FILE
+	echo "	Error building data-service"
 	exit 1
 else
-	echo "	Build done: data-service-$VERSION.tar.gz" >> $LOG_FILE
+	echo "	Build done: data-service-$VERSION.tar.gz"
 fi
 sha512sum data-service-$VERSION.tar.gz > data-service-$VERSION.tar.gz.sha512.txt
 mkdir -p "$RELEASE_PATH/packages/platform/releases/data-service"
 mv data-service-$VERSION.tar.gz $RELEASE_PATH/packages/platform/releases/data-service/data-service-$VERSION.tar.gz
 mv data-service-$VERSION.tar.gz.sha512.txt $RELEASE_PATH/packages/platform/releases/data-service/data-service-$VERSION.tar.gz.sha512.txt
+
+echo "curl -X POST --data-binary @data-service-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/data-service/data-service-$VERSION.tar.gz"
+curl -X POST --data-binary @data-service-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/data-service/data-service-$VERSION.tar.gz
+echo "curl -X POST --data-binary @data-service-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/data-service/data-service-$VERSION.tar.gz.sha512.txt"
+curl -X POST --data-binary @data-service-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/data-service/data-service-$VERSION.tar.gz.sha512.txt
+
 cd ../..
 
 cd hdfs-cleaner
@@ -45,10 +53,12 @@ if [ ! -f hdfs-cleaner-$VERSION.tar.gz ]; then
 	echo "	Error building hdfs-cleaner" >> $LOG_FILE
 	exit 1
 else
-	echo "	Build done: hdfs-cleaner-$VERSION.tar.gz" >> $LOG_FILE
+	echo "	Build done: hdfs-cleaner-$VERSION.tar.gz"
 fi
+
 sha512sum hdfs-cleaner-$VERSION.tar.gz > hdfs-cleaner-$VERSION.tar.gz.sha512.txt
-mkdir -p "$RELEASE_PATH/packages/platform/releases/hdfs-cleaner"
-mv hdfs-cleaner-$VERSION.tar.gz $RELEASE_PATH/packages/platform/releases/hdfs-cleaner/hdfs-cleaner-$VERSION.tar.gz
-mv hdfs-cleaner-$VERSION.tar.gz.sha512.txt $RELEASE_PATH/packages/platform/releases/hdfs-cleaner/hdfs-cleaner-$VERSION.tar.gz.sha512.txt
-cd ../../..
+
+echo "curl -X POST --data-binary @hdfs-cleaner-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/hdfs-cleaner/hdfs-cleaner-$VERSION.tar.gz"
+curl -X POST --data-binary @hdfs-cleaner-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/hdfs-cleaner/hdfs-cleaner-$VERSION.tar.gz
+echo "curl -X POST --data-binary @hdfs-cleaner-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/hdfs-cleaner/hdfs-cleaner-$VERSION.tar.gz.sha512.txt"
+curl -X POST --data-binary @hdfs-cleaner-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/hdfs-cleaner/hdfs-cleaner-$VERSION.tar.gz.sha512.txt
