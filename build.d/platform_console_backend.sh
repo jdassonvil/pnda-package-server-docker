1e4700677d91ddfd17d7c2ed0a4a844f12fb2a3a
Jerome Dassonville <jdassonv@cisco.com>
2016-09-06 11:53:14 +0200

    trying to build another package
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

--- build.d/platform_console_backend.sh
+++ build.d/platform_console_backend.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 PLATFORM_CONSOLE_BACKEND="$GITHUB_ENDPOINT/platform-console-backend.git" 
-echo "Step 6: cloning platform-console-backend $PLATFORM_CONSOLE_BACKEND in $PWD" >> $LOG_FILE
+PACKAGE_SERVER="http://192.168.1.205:3535"
+
+echo "Cloning platform-console-backend $PLATFORM_CONSOLE_BACKEND in $PWD"
 if [ ! -d "$PWD/platform-console-backend" ]; then
 	git clone $PLATFORM_CONSOLE_BACKEND
 	if [ ! -d "$PWD/platform-console-backend" ]; then
-		echo "Error clonning platform-console-backend" >> $LOG_FILE
+		echo "Error clonning platform-console-backend"
 		exit 1
 	fi
 else
-	echo "	getting $BRANCH for platform-console-backend" >> $LOG_FILE
+	echo "	getting $BRANCH for platform-console-backend"
 fi
 cd platform-console-backend
 git checkout $BRANCH
@@ -26,16 +28,22 @@ npm install
 echo "{ \"name\": \"console-backend-data-logger\", \"version\": \"$VERSION\" }" > package-version.json
 grunt --verbose package
 if [ ! -f console-backend-data-logger-$VERSION.tar.gz ]; then
-	echo "	Error building platform-console-backend-data-logger" >> $LOG_FILE
+	echo "	Error building platform-console-backend-data-logger"
 	exit 1
 else
-	echo "	Build done: console-backend-data-logger-$VERSION.tar.gz" >> $LOG_FILE
+	echo "	Build done: console-backend-data-logger-$VERSION.tar.gz"
 fi
 sha512sum console-backend-data-logger-$VERSION.tar.gz > console-backend-data-logger-$VERSION.tar.gz.sha512.txt
-mkdir -p "$RELEASE_PATH/packages/platform/releases/console"
-mv console-backend-data-logger-$VERSION.tar.gz $RELEASE_PATH/packages/platform/releases/console/console-backend-data-logger-$VERSION.tar.gz
-mv console-backend-data-logger-$VERSION.tar.gz.sha512.txt $RELEASE_PATH/packages/platform/releases/console/console-backend-data-logger-$VERSION.tar.gz.sha512.txt
+
+# Publish to package server
+echo "curl -X POST -o console-backend-data-logger-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/platform-testing/console-backend-data-logger-$VERSION.tar.gz"
+curl -X POST -o console-backend-data-logger-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/platform-testing/console-backend-data-logger-$VERSION.tar.gz
+echo "curl -X POST -o console-backend-data-logger-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/platform-testing/console-backend-data-logger-$VERSION.tar.gz.sha512.txt"
+curl -X POST -o console-backend-data-logger-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/platform-testing/console-backend-data-logger-$VERSION.tar.gz.sha512.txt
+
 cd ..
+
+
 cp -R console-backend-utils console-backend-data-manager/
 mkdir -p console-backend-data-manager/conf
 echo "Version  $VERSION" >> console-backend-data-manager/MANIFEST
@@ -44,13 +52,15 @@ npm install
 echo "{ \"name\": \"console-backend-data-manager\", \"version\": \"$VERSION\" }" > package-version.json
 grunt package --verbose
 if [ ! -f console-backend-data-manager-$VERSION.tar.gz ]; then
-	echo "	Error building platform-console-backend-data-manager" >> $LOG_FILE
+	echo "	Error building platform-console-backend-data-manager"
 	exit 1
 else
-	echo "	Build done: console-backend-data-manager-$VERSION.tar.gz" >> $LOG_FILE
+	echo "	Build done: console-backend-data-manager-$VERSION.tar.gz"
 fi
-sha512sum console-backend-data-manager-$VERSION.tar.gz > console-backend-data-manager-$VERSION.tar.gz.sha512.txt
-mv console-backend-data-manager-$VERSION.tar.gz $RELEASE_PATH/packages/platform/releases/console/console-backend-data-manager-$VERSION.tar.gz
-mv console-backend-data-manager-$VERSION.tar.gz.sha512.txt $RELEASE_PATH/packages/platform/releases/console/console-backend-data-manager-$VERSION.tar.gz.sha512.txt
-cd ../..
 
+# Publish to package server
+
+echo "curl -X POST -o console-backend-data-manager-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/platform-testing/console-backend-data-manager-$VERSION.tar.gz"
+curl -X POST -o console-backend-data-manager-$VERSION.tar.gz $PACKAGE_SERVER/packages/platform/releases/platform-testing/console-backend-data-manager-$VERSION.tar.gz
+echo "curl -X POST -o console-backend-data-manager-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/platform-testing/console-backend-data-manager-$VERSION.tar.gz.sha512.txt"
+curl -X POST -o console-backend-data-manager-$VERSION.tar.gz.sha512.txt $PACKAGE_SERVER/packages/platform/releases/platform-testing/console-backend-data-manager-$VERSION.tar.gz.sha512.txt
