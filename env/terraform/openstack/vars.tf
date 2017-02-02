5bd2b1caa9df1f67920c9f47f8b87d0af10dcbb7
Jerome Dassonville <jdassonv@cisco.com>
2017-02-02 13:12:32 +0100

    Fix vars format
    Signed-off-by: Jerome Dassonville <jdassonv@cisco.com>

--- env/terraform/openstack/vars.tf
+++ env/terraform/openstack/vars.tf
@@ -1,50 +1,39 @@
-{
-  "variable": {
-    "image": {
-      "type": : "string",
-      "description": "the image name"
-    }
-  },
-  "variable": {
-    "flavor": {
-      "type": : "string",
-      "description": "the instance flavor, should be at least 4GB RAM and 2 VCPU"
-    }
-  },
-  "variable": {
-    "id_rsa": {
-      "type": : "string",
-      "description": "the public rsa key"
-    }
-  },
-  "variable": {
-    "user_name": {
-      "type": : "string",
-      "description": "username credential for openstack api"
-    }
-  },
-  "variable": {
-    "tenant_name": {
-      "type": : "string",
-      "description": "targeted tenant for openstack api"
-    }
-  },
-  "variable": {
-    "password": {
-      "type": : "string",
-      "description": "password credential for openstack api"
-    }
-  },
-  "variable": {
-    "auth_url": {
-      "type": : "string",
-      "description": "authentication endpoint of openstack api"
-    }
-  },
-  "variable": {
-    "router_id": {
-      "type": : "string",
-      "description": "public router id"
-    }
-  }
+variable "tenant_name" {
+  type = "string"
+  description = "targeted tenant for openstack api"
+}
+
+variable "auth_url" {
+  type = "string"
+  description = "authentication endpoint of openstack api"
+}
+
+variable "user_name" {
+  type = "string"
+  description = "username credential for openstack api"
+}
+
+variable "password" {
+  type = "string"
+  description = "password credential for openstack api"
+}
+
+variable "image" {
+  type = "string"
+  description = "the operating system image name, RHEL/Centos is preferred"
+}
+
+variable "flavor" {
+  type = "string"
+  description = "the instance flavor, should be at least 4GB RAM and 2 VCPU"
+}
+
+variable "id_rsa" {
+  type = "string"
+  description = "the public rsa key"
+}
+
+variable "router_id" {
+  type = "string"
+  description = "public router id"
 }
